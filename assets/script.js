8a317a658a75b2711175277c0a0fc6e9a310251c
Madison Vega <madison.r.vega@gmail.com>
2021-02-27 18:35:53 -0600

    added classes to html to match up with css, created variables to get a handle on the text input, added input and buttons to html

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// shows current day and time in html
+var today = moment().format('dddd MMMM Do, YYYY, HH:mm a');
+$('#currentDay').text(today);
+
+// variables to get handle on text input
+var input9 = $('#9').value();
+var input10 = $('#10').value();
+var input11 = $('#11').value();
+var input12 = $('#12').value();
+var input13 = $('#13').value();
+var input14 = $('#14').value();
+var input15 = $('#15').value();
+var input16 = $('#16').value();
+var input17 = $('#17').value();
+
