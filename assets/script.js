0c095f788aeebb18b386a5fc21af68ed51e65b1b
Madison Vega <madison.r.vega@gmail.com>
2021-02-28 19:17:24 -0600

    added ids to save buttons and got a handle on them in javascript

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,23 @@ var today = moment().format('dddd MMMM Do, YYYY, HH:mm a');
 $('#currentDay').text(today);
 
 // variables to get handle on text input
-var input9 = $('#9').value();
-var input10 = $('#10').value();
-var input11 = $('#11').value();
-var input12 = $('#12').value();
-var input13 = $('#13').value();
-var input14 = $('#14').value();
-var input15 = $('#15').value();
-var input16 = $('#16').value();
-var input17 = $('#17').value();
+var input9 = $('#9').val('');
+var input10 = $('#10').val('');
+var input11 = $('#11').val('');
+var input12 = $('#12').val('');
+var input13 = $('#13').val('');
+var input14 = $('#14').val('');
+var input15 = $('#15').val('');
+var input16 = $('#16').val('');
+var input17 = $('#17').val('');
+
+var saveBtn9 = $('#savebtn9');
+var saveBtn10 = $('#savebtn10');
+var saveBtn11 = $('#savebtn11');
+var saveBtn12 = $('#savebtn12');
+var saveBtn13 = $('#savebtn13');
+var saveBtn14 = $('#savebtn14');
+var saveBtn15 = $('#savebtn15');
+var saveBtn16 = $('#savebtn16');
+var saveBtn17 = $('#savebtn17');
 
